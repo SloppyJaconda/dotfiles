a3f9fad05fa542cfa2e0d94485456cf6b03a1efb
Jordan Maples <jordan.maples@gmail.com>
2025-12-29 17:25:30 -0800

    some stuff I was doing before christmas, i don't know how useful any of it is

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,25 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	keys = function()
-		return {
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-		}
-
-	end,
+  "nvim-telescope/telescope.nvim",
+  keys = function()
+    return {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    }
+  end,
+  config = function()
+    require('telescope').setup{
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+        },
+      }
+    }
+  end
 }
 
