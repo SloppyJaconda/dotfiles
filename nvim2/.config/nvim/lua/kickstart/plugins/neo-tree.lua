72ca95459c95c0aa539c7cf16dc7cc078c5b9339
Jordan Maples <jordan.maples@gmail.com>
2026-01-04 12:50:00 -0800

    get neo-tree to show hiddens by default. google ai code suggestion

--- nvim2/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim2/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,28 +2,34 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    require('neo-tree').setup(opts)
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignore = true,
+				hide_hidden = false,
+			},
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+	end,
 }
